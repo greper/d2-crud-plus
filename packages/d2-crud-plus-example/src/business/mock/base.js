46fc8c0f34c1124577acdf63a9969ec1986012d5
xiaojunnuo <252959493@qq.com>
2019-10-21 19:03:46 +0800

    增加file-uploader扩展

---
+++ packages/d2-crud-plus-example/src/business/mock/base.js
@@ -0,0 +1,76 @@
+export default {
+  buildMock (options) {
+    let name = options.name
+    let list = options.list
+    return [
+      {
+        path: 'api/' + name + '/page',
+        method: 'get',
+        handle (req) {
+          return {
+            code: 0,
+            msg: 'success',
+            data: {
+              records: list,
+              total: list.length,
+              size: 20,
+              current: list.length % 20 === 0 ? list.length / 20 : list.length / 20 + 1
+            }
+          }
+        }
+      },
+      {
+        path: 'api/' + name + '/add',
+        method: 'post',
+        handle (req) {
+          req.body.id = ++options.idGenerator
+          list.push(req.body)
+          console.log('req', req, list)
+          return {
+            code: 0,
+            msg: 'success',
+            data: null
+          }
+        }
+      },
+      {
+        path: 'api/' + name + '/update',
+        method: 'post',
+        handle (req) {
+          for (let item of list) {
+            if (item.id === req.body.id) {
+              Object.assign(item, req.body)
+              break
+            }
+          }
+          console.log('req', req, list)
+          return {
+            code: 0,
+            msg: 'success',
+            data: null
+          }
+        }
+      },
+      {
+        path: 'api/upload/delete',
+        method: 'post',
+        handle (req) {
+          for (let i = 0; i < list.length; i++) {
+            let item = list[i]
+            if ((item.id) === parseInt(req.params.id)) {
+              console.log('remove i')
+              list.splice(i, 1)
+              break
+            }
+          }
+          console.log('req', req, list)
+          return {
+            code: 0,
+            msg: 'success',
+            data: null
+          }
+        }
+      }
+    ]
+  }
+}
--- packages/d2-crud-plus-example/src/business/views/form/uploader/mock.js
+++ packages/d2-crud-plus-example/src/business/views/form/uploader/mock.js
@@ -1,70 +1,23 @@
-let idGenerator = 0
+import mockUtil from '../../../mock/base'
+
+let options = {
+  name: 'upload',
+  idGenerator: 0
+}
 const list = [
   {
-    id: ++idGenerator,
+    id: ++options.idGenerator,
     avatar: 'http://qiniu.veryreader.com/extends/avatar.jpg',
     file: 'http://qiniu.veryreader.com/extends/avatar.jpg',
     image: 'http://qiniu.veryreader.com/extends/avatar.jpg'
   },
   {
-    id: ++idGenerator,
+    id: ++options.idGenerator,
     avatar: 'http://qiniu.veryreader.com/extends/avatar.jpg',
     file: 'http://qiniu.veryreader.com/extends/avatar.jpg',
     image: 'http://qiniu.veryreader.com/extends/avatar.jpg'
   }
 ]
-const page = {
-  records: list,
-  total: 11,
-  size: 20,
-  current: 1
-}
-export default [
-  {
-    path: 'api/upload/page',
-    method: 'get',
-    handle (req) {
-      return {
-        code: 0,
-        msg: 'success',
-        data: page
-      }
-    }
-  },
-  {
-    path: 'api/upload/add',
-    method: 'post',
-    handle (req) {
-      console.log(req)
-      req.body.id = ++idGenerator
-      list.push(req.body)
-      return {
-        code: 0,
-        msg: 'success',
-        data: null
-      }
-    }
-  },
-  {
-    path: 'api/upload/update',
-    method: 'post',
-    handle (req) {
-      return {
-        code: 0,
-        msg: 'success',
-        data: null
-      }
-    }
-  },
-  {
-    path: 'api/upload/delete',
-    method: 'post',
-    handle (req) {
-      return {
-        code: 0,
-        msg: 'success',
-        data: null
-      }
-    }
-  }
-]
+options.list = list
+let mock = mockUtil.buildMock(options)
+export default mock

e546b59a81c15e3bb1f24e617b01c0146a7253d3
xiaojunnuo <xiaojunnuo@yonsz.com>
2019-10-30 14:40:29 +0800

    file uploader 懒加载

--- packages/d2-crud-plus-extends/vue.config.js
+++ packages/d2-crud-plus-extends/vue.config.js
@@ -1,11 +1,16 @@
+const webpack = require('webpack')
+const LimitChunkCountPlugin = new webpack.optimize.LimitChunkCountPlugin({
+  maxChunks: 1,
+  minChunkSize: 1000
+})
+const plugins = []
+plugins.push(LimitChunkCountPlugin)
 module.exports = {
   css: { extract: false },
   configureWebpack: {
     externals: {
       'vue': 'Vue'
     },
-    output: {
-      filename: 'bundle.js'
-    }
+    plugins: plugins
   }
 }

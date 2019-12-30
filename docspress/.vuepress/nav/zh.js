30dd26b66fd5bc360051bfce4513076bf566d224
xiaojunnuo <252959493@qq.com>
2019-12-30 18:45:31 +0800

    docs: vuepress整理文档

--- docspress/.vuepress/nav/zh.js
+++ docspress/.vuepress/nav/zh.js
@@ -3,5 +3,5 @@ module.exports = [
     text: '指南',
     link: '/guide/'
   },
-  { text: '演示', link: 'qiniu.veryreader.com/D2CrudPlusExample/index.html' },
+  { text: '演示', link: 'http://qiniu.veryreader.com/D2CrudPlusExample/index.html' },
 ]

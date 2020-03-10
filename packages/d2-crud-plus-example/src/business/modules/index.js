0c345f29e1dcab9f54c63e986c0c0b2617c7c69c
xiaojunnuo <252959493@qq.com>
2020-03-10 19:21:03 +0800

    perf: 重构菜单路由的加载方式

--- packages/d2-crud-plus-example/src/business/modules/index.js
+++ packages/d2-crud-plus-example/src/business/modules/index.js
@@ -3,7 +3,19 @@ import './permission' // 加载permission
 import { frameInChildren } from '@/router/routes'
 import asideMenu from '@/menu/aside'
 import headerMenu from '@/menu/header'
+import router from '@/router'
+import layoutHeaderAside from '@/layout/header-aside'
 // 导出静态菜单和路由
 asideMenu.push(...exampleModule.menus.aside)
 headerMenu.push(...exampleModule.menus.header)
 frameInChildren.push(...exampleModule.routers.frameIn)
+
+const addRoutes = [
+  {
+    path: '/',
+    redirect: { name: 'index' },
+    component: layoutHeaderAside,
+    children: exampleModule.routers.frameIn
+  }
+]
+router.addRoutes(addRoutes)

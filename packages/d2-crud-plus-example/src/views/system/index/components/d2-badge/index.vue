05d699af521928b13c4ea62bb2f90b69de547b22
xiaojunnuo <252959493@qq.com>
2020-04-03 11:28:32 +0800

    refactor: 修改徽章图标

--- packages/d2-crud-plus-example/src/views/system/index/components/d2-badge/index.vue
+++ packages/d2-crud-plus-example/src/views/system/index/components/d2-badge/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d2-badge">
     <p align="center">
-      <a><img src="https://img.shields.io/github/release/greper/d2-crud-plus.svg"/></a>
+<!--      <a><img src="https://img.shields.io/github/release/greper/d2-crud-plus.svg"/></a>-->
       <a><img src="https://img.shields.io/github/forks/greper/d2-crud-plus.svg"/></a>
       <a><img src="https://img.shields.io/github/stars/greper/d2-crud-plus.svg"/></a>
       <a><img src="https://img.shields.io/github/issues/greper/d2-crud-plus.svg"/></a>
@@ -12,7 +12,7 @@
     <p align="center">
 <!--      <a><img src="https://img.shields.io/npm/v/@d2-admin/ice-scaffold.svg"/></a>-->
 <!--      <a href="https://www.travis-ci.org/greper/d2-crud-plus-example"><img src="https://www.travis-ci.org/greper/d2-crud-plus-example.svg?branch=master"/></a>-->
-      <a><img src="https://img.shields.io/github/last-commit/greper/d2-crud-plus-example.svg"/></a>
+      <a><img src="https://img.shields.io/github/last-commit/greper/d2-crud-plus.svg"/></a>
       <a><img src="https://img.shields.io/badge/code_style-standard-brightgreen.svg"/></a>
 <!--      <a><img src="https://api.netlify.com/api/v1/badges/a5dd4bbd-da3f-4145-98a9-8012577bdcf5/deploy-status"/></a>-->
     </p>

5ee4ea2b280f4963ee18585850fb39b4af595669
xiaojunnuo <252959493@qq.com>
2020-06-17 15:50:24 +0800

    fix: 修复手机号输入组件没有加载的bug

--- packages/d2-crud-plus-example/src/business/components/index.js
+++ packages/d2-crud-plus-example/src/business/components/index.js
@@ -1,6 +1,7 @@
 import d2Admin from './d2-admin'
 import Vue from 'vue'
+import { ElPhoneNumberInput } from 'el-phone-number-input'
 Vue.use(d2Admin)
 Vue.component('link-button', () => import('./link-button/index.vue'))
 Vue.component('example-helper', () => import('./example-helper/index.vue'))
-Vue.component('el-phone-number-input', () => import('el-phone-number-input'))
+Vue.component('el-phone-number-input', ElPhoneNumberInput)

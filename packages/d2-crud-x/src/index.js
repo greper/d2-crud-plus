2e61b53d746cda80fc7ed1d63db5c1921b599bb2
xiaojunnuo <25299493@qq.com>
2020-06-26 00:06:13 +0800

    fix: 支持自定义d2-crud的标签名称

--- packages/d2-crud-x/src/index.js
+++ packages/d2-crud-x/src/index.js
@@ -1,6 +1,10 @@
 import d2Crud from './d2-crud.vue'
 const install = (Vue, options) => {
-  Vue.component('d2Crud', d2Crud)
+  let name = 'd2Crud'
+  if (options && options.name != null) {
+    name = options.name
+  }
+  Vue.component(name, d2Crud)
 }
 if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue)

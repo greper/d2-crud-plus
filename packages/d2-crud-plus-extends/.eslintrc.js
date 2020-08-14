f5f7834ae92e1900e589f06145b3b117b319783f
xiaojunnuo@126.com <xiaojunnuo@qq.com>
2020-08-14 14:17:21 +0800

    refactor: 格式化

--- packages/d2-crud-plus-extends/.eslintrc.js
+++ packages/d2-crud-plus-extends/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
+  'extends': [
     'plugin:vue/essential',
     '@vue/standard'
   ],
@@ -15,3 +15,4 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
+

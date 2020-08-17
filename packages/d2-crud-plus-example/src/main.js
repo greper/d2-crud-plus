deaf9b37789febc2eb13f61414541d21ae29de1c
xiaojunnuo@126.com <xiaojunnuo@qq.com>
2020-08-17 18:01:36 +0800

    refactor: 自动化e2e测试

--- packages/d2-crud-plus-example/src/main.js
+++ packages/d2-crud-plus-example/src/main.js
@@ -24,8 +24,9 @@ new Vue({
     this.$store.commit('d2admin/page/init', frameInRoutes)
     // 设置顶栏菜单
     this.$store.commit('d2admin/menu/headerSet', menuHeader)
-    // 设置侧边栏菜单
-    this.$store.commit('d2admin/menu/asideSet', menuHeader[2].children)
+    // 设置默认侧边栏菜单
+    const defaultMenuIndex = menuHeader.length > 2 ? 2 : menuHeader.length
+    this.$store.commit('d2admin/menu/asideSet', menuHeader[defaultMenuIndex].children)
     // 初始化菜单搜索功能
     this.$store.commit('d2admin/search/init', menuHeader)
   },

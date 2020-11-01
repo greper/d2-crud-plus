940e460eb4214c01f455575c8fb71dafeb88d809
xiaojunnuo <xiaojunnuo@qq.com>
2020-11-01 23:00:50 +0800

    docs: 小修改

--- docspress/.vuepress/config.js
+++ docspress/.vuepress/config.js
@@ -165,7 +165,8 @@ function getGuideSidebar (groupA, groupB,groupC) {
         'breakingchange',
         'upgradechange',
         'questions',
-        'contact'
+        'contact',
+        'donate'
       ]
     },
     {
---
+++ docspress/guide/donate.md
@@ -0,0 +1,5 @@
+# 捐赠
+支持维护，给我打打鸡血，抱拳抱拳  
+![](http://d2p.file.veryreader.com/greper/donate.jpg-400_400)
+
+

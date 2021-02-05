feb8830cd268f4e19cc4a134c59b215aab44a368
zhangfc <crstudio@qq.com>
2021-02-05 16:56:12 +0800

    在full-editor下面添加 wangEditor 编辑器

--- packages/d2-crud-plus-extends/src/full-editor/index.js
+++ packages/d2-crud-plus-extends/src/full-editor/index.js
@@ -1,16 +1,21 @@
 import { d2CrudPlus } from '../utils/d2-crud-plus'
 import types from './types'
-import defaultConfig from './lib/d2-ueditor/utils/config'
+import defaultUeditorConfig from './lib/d2-ueditor/utils/config'
+import defaultWangConfig from './lib/d2-wang-editor/utils/config'
 function install (Vue, options) {
   Vue.component('d2p-quill', () => import('./lib/d2-quill'))
   Vue.component('d2p-ueditor', () => import('./lib/d2-ueditor'))
+  Vue.component('d2p-wang-editor', () => import('./lib/d2-wang-editor'))
   // 配置type
   if (d2CrudPlus != null) {
     // 设置默认uploader
     d2CrudPlus.util.columnResolve.addTypes(types)
   }
   if (options && options.ueditor) {
-    Object.assign(defaultConfig, options.ueditor)
+    Object.assign(defaultUeditorConfig, options.ueditor)
+  }
+  if (options && options.wangEditor) {
+    Object.assign(defaultWangConfig, options.wangEditor)
   }
 }
 
--- packages/d2-crud-plus-extends/src/full-editor/types.js
+++ packages/d2-crud-plus-extends/src/full-editor/types.js
@@ -4,6 +4,9 @@ export default {
   },
   'editor-ueditor': {
     form: { component: { name: 'd2p-ueditor', span: 24 } }
+  },
+  'editor-wang': {
+    form: { component: { name: 'd2p-wang-editor', span: 24 } }
   }
 
 }

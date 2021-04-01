a33680d880be3ec913030de9ba956be40aef5393
xiaojunnuo <xiaojunnuo@qq.com>
2021-04-01 10:23:57 +0800

    refactor: 1

--- packages/d2-crud-plus-example/vue.config.js
+++ packages/d2-crud-plus-example/vue.config.js
@@ -178,5 +178,5 @@ module.exports = {
     }
   },
   // 解决ie打不开的问题
-  transpileDependencies: ['d2p-extends','@d2-projects/vue-table-export']
+  transpileDependencies: ['d2p-extends', '@d2-projects/vue-table-export']
 }
